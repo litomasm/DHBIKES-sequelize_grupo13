b3f7780d22bd7b938ed44dcfe55435626ead4585
litomasm <litomasm2006@hotmail.com>
2021-01-11 02:01:36 -0300

    funcionalidades productos, falta pulir

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -18,7 +18,58 @@ let productosController = {
 
         });
         res.redirect("/")
-    }
-}
+    },
 
-module.exports = productosController;
+    list: (req, res, next) => {
+        db.Product.findAll().then((products)=>{
+            res.render("allProducts", {products})
+        })
+    },
+
+    detail: async (req, res) => {
+        const id = req.params.id;
+        const product = await db.Product.findByPk(id, {
+            include: [{association:"category"}]
+        })
+        
+        res.render("producto", {product});
+    },
+
+    editar: function (req, res){
+        let pedidoProducto = db.Product.findByPk(req.params.id);
+        let pedidoCategory = db.Category.findAll();
+
+        Promise.all([pedidoProducto, pedidoCategory])
+        .then(function([product, categorys]){
+
+            res.render("productoEdit", {product:product, categorys: categorys})
+        })
+    },
+
+    actualizar: function (req, res){
+        db.Product.update({
+            name: req.body.name,
+            price: req.body.price,
+            category_id: req.body.category,
+            description: req.body.description,
+        }, {
+            where:{
+                id:req.params.id
+            }
+
+        });
+
+        res.redirect("/products/detail/" + req.params.id)
+    },
+
+    borrar: function (req, res){
+        db.Product.destroy({
+            where:{
+                id: req.params.id
+            }
+        })
+
+        res.redirect("/products/");
+}
+}
+module.exports = productosController
